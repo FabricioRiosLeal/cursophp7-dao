8a39a0e08ec2ad24b415608a99a4e66736a0d062
FabricioRiosLeal <fabricioriosleal@gmail.com>
2018-11-24 13:32:31 -0200

    Metodos Lista

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 
 require_once("config.php");
 
-$root = new usuario();
+//Carrega um usuário
+//$root = new usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(3);
+//carrega uma lista de usuários
+//$lista = Usuario::getlist();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("us");
+//echo json_encode($search);
 
+//Carrega usuario usando login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "&%$");
+
+echo $usuario;
 
 ?>
